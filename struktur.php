c9681f91a66918db781372d52c2cd32e1ea5117c
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-01-25 20:43:59 +0700

    struktur.php
    Contoh laman terstruktur

---
+++ struktur.php
@@ -0,0 +1,95 @@
+<?php
+ include 'header.php';
+?>
+<!doctype html>
+	
+	<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+     <link href="gaya/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="gaya/body.css">
+    <style>
+    	.container h1,.container p{
+    	  color:white;
+    	}
+       #tobi p{
+       	color:black;
+       }
+    	body{
+         	background-color:#e4f5ff;
+         }
+         .jumbotron{
+	          background-image: url('gambar/kampus2.png');
+	          background-size:cover;
+	          width:100%;
+	          height:60%;
+	          background-repeat:no-repeat;
+	         text-align:center;
+	          
+            }
+    </style>
+    </head>
+    <body>
+    	<!-- body -->
+    	<div style="margin-top:20px;" class="jumbotron bg-primary"> 
+    	 <div class="container" >
+    	   <br><br> <h1 class="display-4">Struktural</h1>
+           <p class="lead">kampus<br> Universitas Muhammadiyah Bandung</p><div class="garis"></div><br>
+ 	        <br>
+         <a class="btn btn-primary btn-lg font-weight-bold" href="#" role="button">Daftar</a>
+           <br>
+       	<br>
+    	
+          </div>
+    	  
+    
+    	</div>
+   
+    <div class="container">
+    	<img src="gambar/struk.png" width="100%">
+    <br>
+    	<!-- struktural -->
+    	<div>
+    	</div></div><div pagelayer-id="xqc7050" class="p-xqc7050 pagelayer-text">
+<div class="pagelayer-text-holder">
+	<label><font size="18px">Struktur kepengurusan<br> 2014-2022</font></label>
+	<br>
+<div><b>Ketua:</b><br /></div><div>Prof. Dr. Ir. Ellyza Nurdin, MS.<br /><span><br /></span><b><span>Keuangan</span><span>:</span></b></div><div>Sawalni, SPd.,MM<br /><br /></div><div><b>Sekretaris Bidang Penelitian<span>:</span></b></div><div>Dr. Dwintha Lestari, S.Farm., M.Si., Apt.<br /><br /></div><div><b>Koordinator Pusat Research &amp; Jurnal<span>:</span></b></div><div>Dr. Khairiah,SP.,MT<br /></div><div><br /></div>
+</div></div><div pagelayer-id="eno5619" class="p-eno5619 pagelayer-text">
+<div class="pagelayer-text-holder">
+<div><b>Sekretaris Bidang Pengabdian:</b><br /></div><div>Dra. Maelita Ramdani Moeis PhD.<br /><br /></div><div><b>Koordinator PPM<span>:</span></b></div><div>Wandy Zulkarnaen, SE.,SSy., MM., CHRA<br /><br /></div><div><b>Koordinator KKN<span>:</span></b></div><div>Alghif Aruni Nur Rukman, SP.,MSi<br /><br /></div>
+</div></div><div pagelayer-id="fzc5878" class="p-fzc5878 pagelayer-text">
+<div id="tobi" class="pagelayer-text-holder">
+<p><strong><span>Pusat HKI</span><span>:</span><br /></strong></p><p><strong>Kordinator Bid. Diseminasi dan Informasi<span>:</span></strong></p><p>Reza Fikri Alfatah, SP.,MSc.<br /><br /></p><p><strong>Kordinator Bid. Publikasi dan KI<span>:</span></strong></p><p>Putri W.Harlina, SPt.,MSi.,M.Eng.,PhD.<br /><br /></p><p><strong>Kordinator Bid. Inovasi dan Inkubasi<span>:</span></strong></p><p>Saepul Adnan, SSi.,MSi.<br /><strong><br /></strong></p><p><strong>Sekretariat Divisi Hukum<span>:</span></strong></p><p>M. Hilal Nu’man, SH.,MKn.<br /><br /></p>
+</div></div><div pagelayer-id="yoo4887" class="p-yoo4887 pagelayer-text">
+<div class="pagelayer-text-holder">
+<div><b><span style="font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit;">Sistem Informasi dan Publikasi</span><span style="font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit;">:</span></b><br /></div><div>Firas Atqiya, S.Si, M.Si, M.Sc<br /><br /></div><div><b>Sistem Data dan Penilaian Kesiapan Saintek<span>:</span></b></div><div>Fahmi Iman Fahrudin, STP.,MoFT<br /><br /></div><div><b>Sekretariat LP3M<span>:</span></b></div><div>Rivaldi</div>
+</div></div></div></div></div></div>	</div>
+<!-- .entry-content -->
+
+    	</div>
+    </div>
+    <br><br><br><br><br><br>
+    
+    	<!-- footer -->
+    	<div id="pantat" class="container bg-dark fixed-bottom">
+     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="text-muted">uchiha obITo - Razan &copy; 2022</span>
+    </div>
+
+    <div id="pantat2" >
+      <a class="text-muted" href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="locate"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="caret-forward-circle"></ion-icon></a>
+      </div>
+  </footer>
+  </div>
+    </body>
+    
+    </html>
