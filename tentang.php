5ffddb85008186b093990d61d8d1882494809f16
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-01-25 20:44:39 +0700

    tentang.php
    Contoh laman tentang

---
+++ tentang.php
@@ -0,0 +1,142 @@
+<?php
+ include 'header.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+     <link href="gaya/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="gaya/body.css">
+    <style>
+    	.jumbotron{
+	          background-size:cover;
+	          width:100%;
+	          height:60%;
+	          background-repeat:no-repeat;
+	         text-align:center;
+	          
+            }
+         .jumbotron h1,p,h3,h5{
+         	color:bg-dark;
+             font-weight:bold;
+         }
+         body{
+         	background-color:#e4f5ff;
+         }
+    .jumbotron btn{
+	border-radius:45px;
+	padding-right:45px;
+	padding-left:45px;
+	}
+	footer{
+		margin-bottom:45vh;
+	}
+	.info-meta{
+		color:white;
+		}
+    </style>
+    </head>
+    <body>
+    	<!-- content -->
+    	<div style="margin-top:20px;" class="jumbotron bg-dark"> 
+  <div class="container" >
+ <br><br> <h1 class="display-4"><font color="white">Tentang Kami</font></h1>
+ <div class="garis"></div>
+ <p class="lead"><font size="3px" color="white">Pimpinan Wilayah Muhammadiyah Jawa Barat membentuk kembali Panitia Pendirian Universitas Muhammadiyah Bandung. Alhamdulillah, pada tahun tersebut banyak respon positif dari berbagai pihak, khususnya dari para pendiri Salman ITB yang mampu mempertemukan Tim Pendirian Universitas Muhammadiyah Bandung dengan Direktur Kelembagaan Direktoran Pendidikan Tinggi Kementerian Pendidikan dan Kebudayaan.<br>Pada tahun yang sama, Tim Pendirian Universitas Muhammadiyah Bandung melakukan akselerasi sumber daya manusia untuk menggarap hal-hal yang berkaitan dengan pendirian Universitas, di antaranya perumusan dan penyusunan dokumen pengajuan pendirian Universitas Muhammadiyah Bandung. Adapun Program Studi yang diajukan di antaranya yaitu: <b>A. Kelompok Sains dan Teknologi (Saintek)</b>: Teknik Industri, Teknik Elektro, Teknik Informatika, Teknik Lingkungan, Teknologi Pangan Halal, Bioteknologi, dan Farmasi. <b>B. Kelompok Sosial Humaniora</b>: Psikologi, Ilmu Komunikasi, Kriya Tekstil dan Fashion, Agribisnis, dan Administrasi Publik. Hasil evaluasi Tim Kopertis Wilayah IV Jawa Barat dan Banten, dari 12 (dua belas) Program Studi yang diusulkan untuk mendapatkan rekomendasi, ternyata Program Studi Teknik Lingkungan tidak lulus rekomendasi.</font></p>
+
+<br>
+ 	<br>
+ <!-- <a class="btn btn-primary btn-lg font-weight-bold" href="#" role="button">Daftar</a> -->
+ <br>
+ 	<br>
+ </div>
+</div>
+    	<!-- penutup jumbotron -->
+    <div align="center" class="jumbotron bg-dark">
+    	<div class="container">
+    	<div class="card" style="width: 18rem;" style="margin-left:150vh;">
+  <img src="gambar/hakim.jpg" width="50%" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Hai.. 😎</h5>
+    <p class="card-text"><font size="2px">Saya Biasa dipanggil Hakim</font></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><font size="2px">Nama : Muhammad Abdul Hakim</font></li>
+    <li class="list-group-item"><font size="2px">Prodi : Informatika C</font></li>
+    <li class="list-group-item"><font size="2px">Kampus : Universitas Muhammadiyah Bandung</font></li>
+  </ul>
+  <div class="card-body" align="center">
+    <a href="https://wa.me/+6281322621702" class="card-link"><ion-icon name="logo-whatsapp"></ion-icon></a>
+    <a href="#" class="card-link"><ion-icon name="logo-instagram"></ion-icon></a>
+  </div>
+</div>
+<br><br>
+<div class="card" style="width: 18rem;" style="margin-left:150vh;">
+  <img src="gambar/razor.jpeg" width="50%" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Hai.. 😎</h5>
+    <p class="card-text"><font size="2px">Saya Biasa dipanggil Razan</font></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><font size="2px">Nama : Muhammad Razan Rizqullah</font></li>
+    <li class="list-group-item"><font size="2px">Prodi : Informatika A</font></li>
+    <li class="list-group-item"><font size="2px">Kampus : Universitas Muhammadiyah Bandung</font></li>
+  </ul>
+  <div class="card-body" align="center" >
+    <a href="https://wa.me/+6287822002484" class="card-link"><ion-icon name="logo-whatsapp"></ion-icon></a>
+    <a href="#" class="card-link"><ion-icon name="logo-instagram"></ion-icon></a>
+  </div>
+</div>
+<br><br><br>
+<div class="card" style="width: 18rem;" style="margin-left:150vh;">
+  <img src="gambar/kiki.jpg" width="50%" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Hai.. 😎</h5>
+    <p class="card-text"><font size="2px">Saya Biasa dipanggil Kiki</font></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><font size="2px">Nama : Kiki Rizki Ramdani</font></li>
+    <li class="list-group-item"><font size="2px">Prodi : Informatika C</font></li>
+    <li class="list-group-item"><font size="2px">Kampus : Universitas Muhammadiyah Bandung</font></li>
+  </ul>
+  <div class="card-body" align="center" >
+    <a href="https://wa.me/+6281313254257" class="card-link"><ion-icon name="logo-whatsapp"></ion-icon></a>
+    <a href="#" class="card-link"><ion-icon name="logo-instagram"></ion-icon></a>
+  </div>
+</div>
+</div>
+<br><br>
+<!-- map -->
+	<div class="card-body">
+			<h4> <font color="white">Lokasi Kampus</font><h4>
+		</div>
+		<div class="garis"></div>
+	<div class="ratio ratio-4x3" >
+		
+  <div style="padding:10px;"><iframe width="100%" height="75%" id="gmap_canvas" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Bandung%20jl%20Soekarno%20Hatta%20universitas%20Muhammadiyah%20Bandung+(Universitas%20Muhamadiyah%20Bandung)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
+</div><ion-icon name="walk-outline"></ion-icon><p align="left"><font color="white">Alamat : Jl. Soekarno Hatta, Bandung <br> No.Telp : 0821-xxxx-xxx<br>Kelompok 11 is the best</font></p>
+			
+ 
+<!-- <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=""><a href="https://www.gps.ie/golf-gps/">golf range finders</a></iframe></div>-->
+      <!-- akhiran map -->
+<br><br><br><br><br><br><br><br><br>
+    	<!-- footer -->
+<div id="pantat" class="container bg-dark fixed-bottom">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="text-muted">kelompok 11 - kelompok 11 &copy; 2022</span>
+    </div>
+
+    <div id="pantat2" >
+      <a class="text-muted" href="#"><ion-icon name="logo-youtube"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="logo-github"></ion-icon></a>
+      </div>
+  </footer>
+  </div>
+    </body>
+    </html>
