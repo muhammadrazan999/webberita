b4f277611cc6da93dba5fc16ed4462df0dc4bc53
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-01-25 20:41:11 +0700

    berita-1.php
    Contoh konten berita yang pertama

---
+++ berita-1.php
@@ -0,0 +1,134 @@
+<?php
+ include 'header.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+     <link href="gaya/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="gaya/body.css">
+    <style>
+    	.jumbotron{
+	          background-image: url('gambar/kampus2.png');
+	          background-size:cover;
+	          width:100%;
+	          height:60%;
+	          background-repeat:no-repeat;
+	         text-align:center;
+	          
+            }
+         .jumbotron h1,p,h3,h5{
+         	color:white;
+             font-weight:bold;
+         }
+         body{
+         	background-color:#e4f5ff;
+         }
+    .jumbotron btn{
+	border-radius:45px;
+	padding-right:45px;
+	padding-left:45px;
+	}
+	footer{
+		margin-bottom:45vh;
+	}
+	.info-meta{
+		color:white;
+		}
+    </style>
+    </head>
+    <body>
+    	<!-- content -->
+    	<div style="margin-top:150px; margin-bottom:150px;" class="bg-dark"> 
+		<div class="container">
+		<h3>Berita Terbaru</h3>
+		<div class="garis"></div>
+		<!-- konten 1 -->
+			
+			<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-md-9">
+              <div class="panel panel-default">
+               <div class="panel-body">
+                  <h5>Info Terkini Beasiswa</h5>
+                    <div class="info-meta">
+                        <ul class="list-inline">
+                            <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </li>
+                            <li><i class="fa fa-eye"></i>21k</li>
+                            <li><i class="fa fa-heart-o"></i>372</li>
+                            <li><i class="fa fa-user"></i> diposting oleh UMBandung</li>
+                            <li class="pull-right">Kategori : Beasiswa</li>
+                        </ul>
+                    </div>
+                  <hr>
+                  
+                    <div class = "media">
+                       <a class = "pull-left" href = "#">
+                          <img class = "media-object " src = "gambar/berita-1.png" width="100%" >
+                       </a>
+                       <div class = "media-body">
+                       	<br><br>
+                          <p align="left">Mau kuliah di Bioteknologi @umbandung tapi terkendala biaya?
+Tenang... ada beasiswa untuk kalian yang berprestasi.<br><br>
+.
+📣 Universitas Muhammadiyah Bandung memberikan beasiswa voucher berupa potongan 50% untuk biaya SPP, SKS, praktikum, dan BPP hingga semester 8
+.
+<br><br>
+Syarat:
+<br><br>
+- Ranking 1-10 atau berprestasi di bidang non akademik selama sekolah di jenjang SMA/sederajat atau kalian yang sudah menjadi kader muda Muhammadiyah
+- Lulusan tahun 2020
+- Mempertahankan IPK minimal 3 selama kuliah di UM Bandung.
+*Diutamakan domisili Jawa Barat (luar Jawa Barat diperbolehkan)
+.<br>
+⏱ Beasiswa ini berlaku hingga 27 Juli 2020.
+Buruan sebelum kehabisan
+.Perlu info lebih lanjut?<br></p>
+               <div class="col-md-3"> 
+               <div class="panel panel-default">
+               <div class="panel-heading">
+              </div>
+               <div class="panel-body">
+                    <div class="recent">
+                        <a href="#"><img class="img-responsive" src="" alt="" /></a>                
+                        <div class="info-meta">
+                            <ul class="list-inline">
+                                <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </a></li>
+                                <li><i class="fa fa-eye"></i>21k</li>
+                                <li><i class="fa fa-heart-o"></i>372</li>
+                            </ul>
+                        </div>
+                        <h6 class="entry-tile">
+                            <a href="berita-2.php"><font size="5px">berita pendaftaran<br>gelombang selanjutnya</font></a>
+                        </h6>
+                    </div>
+                    <!-- open recent -->
+                    
+                    <!-- close recent -->
+                </div>
+                       </div>
+                        </div>
+                    <hr>
+                    <!--recent-->
+			<!-- berita 2 -->
+		</div>
+    	<!-- footer -->
+<div id="pantat" class="container bg-dark fixed-bottom">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="text-muted">kelompok 11 - kelompok 11 &copy; 2022</span>
+    </div>
+
+    <div id="pantat2" >
+      <a class="text-muted" href="#"><ion-icon name="logo-youtube"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="logo-github"></ion-icon></a>
+      </div>
+  </footer>
+  </div>
+    </body>
+    </html>
