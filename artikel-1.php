a0980bb6f9cba55285e8583f582f52641e89636a
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-01-25 20:36:45 +0700

    artikel-1.php
    contoh konten artikel nya

---
+++ artikel-1.php
@@ -0,0 +1,128 @@
+<?php
+ include 'header.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+     <link href="gaya/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="gaya/body.css">
+    <style>
+    	.jumbotron{
+	          background-image: url('gambar/kampus2.png');
+	          background-size:cover;
+	          width:100%;
+	          height:60%;
+	          background-repeat:no-repeat;
+	         text-align:center;
+	          
+            }
+         .jumbotron h1,p,h3,h5{
+         	color:white;
+             font-weight:bold;
+         }
+         body{
+         	background-color:#e4f5ff;
+         }
+    .jumbotron btn{
+	border-radius:45px;
+	padding-right:45px;
+	padding-left:45px;
+	}
+	footer{
+		margin-bottom:45vh;
+	}
+	.info-meta{
+		color:white;
+		}
+    </style>
+    </head>
+    <body>
+    	<!-- content -->
+    	<div style="margin-top:150px; margin-bottom:150px;" class="bg-dark"> 
+		<div class="container">
+		<h3>Berita Terbaru</h3>
+		<div class="garis"></div>
+		<!-- konten 1 -->
+			
+			<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-md-9">
+              <div class="panel panel-default">
+               <div class="panel-body">
+                  <h5>BEM UMBandung Resmi di Lantik</h5>
+                    <div class="info-meta">
+                        <ul class="list-inline">
+                            <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </li>
+                            <li><i class="fa fa-eye"></i>21k</li>
+                            <li><i class="fa fa-heart-o"></i>372</li>
+                            <li><i class="fa fa-user"></i> diposting oleh UMBandung</li>
+                            <li class="pull-right">Kategori : Kemahasiswaan</li>
+                        </ul>
+                    </div>
+                  <hr>
+                  
+                    <div class = "media">
+                       <a class = "pull-left" href = "#">
+                          <img class = "media-object " src = "gambar/kampusbulat.png" width="100%" >
+                       </a>
+                       <div class = "media-body">
+                       	<br><br>
+                          <p align="left">
+ Admin  Mahasiswa
+BANDUNGMU.COM – Bertempat di auditorium utama, kepengurusan Badan Eksekutif Mahasiswa (BEM) Universitas Muhammadiyah Bandung (UMBandung) periode 2021-2022 resmi dilantik pada Kamis (18/11/2021) siang. <br><br>Presiden mahasiswa (presma) atau Ketua BEM periode 2021-2022 Yudi Yudhiana mengatakan banyak program yang akan dilakukan oleh kepengurusannya. Dua di antaranya yakni sayembara melukis desain batik untuk simbol UMBandung dan standardisasi dari legalitas setiap lembaga kemahasiswaan. ”Apa yang menjadi visi dan misi kami, apa yang menjadi harapan sebelumnya, gagasan-gagasan sebelumnya bisa direalisasikan dengan segera,” ucap Yudi. Dalam visi dan misi organisasi, Yudi bersama anggota kepengurusan lainnya ingin membangun kebersaman dengan lembaga kemahasiswaan, himpunan, demi menciptakan kader generasi emas yang intelektual. <br><br>”Kami akan membangun kebersamaan, semangat kolaboratif, dan semangat perjuangan, itulah yang akan kami usahakan supaya lebih baik ke depannya,” lanjutnya. Sementara itu Presma periode 2020-2021 Panca Pebriana Hidayah berpesan kepada kepenegurusan yang baru supaya memiliki rasa menjaga dengan sesama anggota kepengurusan. ”Kami berharap periode yang sekarang untuk IMM ataupun BEM ke depannya memiliki rasa yang independen dalam mengelola lembaga-lembaga kemahasiswaan supaya lembaga kemahasiswaan ini suci dari berbagai kepentingan apa pun,” ujar Panca. <br><br>Selain itu, ia berharap terhadap lembaga mahasiswa tersebut untuk bisa berkontribusi dengan menghasilkan berbagai prestasi yang berarti. ”Kami juga berharap untuk BEM dan IMM tahun ini bisa berkontribusi membawa mahasiswa UMBandung untuk lebih berprestasi di bidang akademik ataupun non-akademik serta bisa berkontribusi dalam gerakan sosial masyarakat serta gerakan sosial politik,” lanjutnya. Kontribusi positif Harapan besar disampaikan Kepala Bagian Kemahasiswaan dan Pengembangan Karier UMBandung Taufik Maulana, S.Pd.I., M.Pd.<br><br> bahwa kepengurusan BEM yang baru bisa konsisten berkontribusi positif untuk kampus UMBandung agar bisa lebih baik dari sebelumnya. ”Tentu yang paling penting adalah harapan kami dari kampus, BEM mampu berbuat positif dan juga banyak merangkul teman-teman mahasiswa, baik itu di UKM, Hima, atau di IMM dari setiap fakultas. Dengan cara seperti itu, kita bisa sama-sama membangun kampus tercinta ini menjadi lebih baik lagi dan jadi kampus yang lebih berkemajuan diiringi prestasi membanggakan,” kata Taufik.<br><br> Taufik berpesan agar teman-teman dari BEM bisa mendukung program kreativitas mahasiswa (PKM) nantinya bisa berpengaruh terhadap peningkatan kualitas kampus.
+”Saya berharap semoga BEM kepengurusan yang baru ini bisa membantu program-program kampus dan bisa menyosialisasikan program kami yang ujung-ujungnya ini adalah dari, oleh, dan untuk kita semua di UMBandung,” turur Taufik.
+Acara ini berlangsung meriah dengan tetap menerapkan protokol kesehatan. <br><br>Turut hadir dalam acara Rektor UMBandung dan para wakil rektor, Ketua PC IMM Kota Bandung, dan beberapa tamu lainnya.
+Selain pelantikan BEM, dalam acara ini juga dilakukan pembukaan Darul Arqam Madya (DAD) Jaya Korkom IMM UMBandung.***(Firman Katon)
+    
+                         </p>
+               <div class="col-md-3"> 
+               <div class="panel panel-default">
+               <div class="panel-heading">
+              </div>
+               <div class="panel-body">
+                    <div class="recent">
+                        <a href="#"><img class="img-responsive" src="" alt="" /></a>                
+                        <div class="info-meta">
+                            <ul class="list-inline">
+                                <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </a></li>
+                                <li><i class="fa fa-eye"></i>21k</li>
+                                <li><i class="fa fa-heart-o"></i>372</li>
+                            </ul>
+                        </div>
+                        <h6 class="entry-tile">
+                            <a href="artikel-2.php"><font size="5px">Webinar Memperingati Hari Pangan Sedunia</font></a>
+                        </h6>
+                    </div>
+                    <!-- open recent -->
+                    
+                    <!-- close recent -->
+                </div>
+                       </div>
+                        </div>
+                    <hr>
+                    <!--recent-->
+			<!-- berita 2 -->
+		</div>
+    
+    
+    	<!-- footer -->
+<div id="pantat" class="container bg-dark fixed-bottom">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="text-muted">kelompok 11 - kelompok 11 &copy; 2022</span>
+    </div>
+
+    <div id="pantat2" >
+      <a class="text-muted" href="#"><ion-icon name="logo-youtube"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="logo-github"></ion-icon></a>
+      </div>
+  </footer>
+  </div>
+    </body>
+    </html>
