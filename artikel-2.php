c997b31a2ce5369b26bd69b773cd3c717402f2ab
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-01-25 20:38:23 +0700

    artikel-2.php
    Jenis atau contoh artikel selanjutnya

---
+++ artikel-2.php
@@ -0,0 +1,131 @@
+<?php
+ include 'header.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+     <link href="gaya/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="gaya/body.css">
+    <style>
+    	.jumbotron{
+	          background-image: url('gambar/kampus2.png');
+	          background-size:cover;
+	          width:100%;
+	          height:60%;
+	          background-repeat:no-repeat;
+	         text-align:center;
+	          
+            }
+         .jumbotron h1,p,h3,h5{
+         	color:white;
+             font-weight:bold;
+         }
+         body{
+         	background-color:#e4f5ff;
+         }
+    .jumbotron btn{
+	border-radius:45px;
+	padding-right:45px;
+	padding-left:45px;
+	}
+	footer{
+		margin-bottom:45vh;
+	}
+	.info-meta{
+		color:white;
+		}
+    </style>
+    </head>
+    <body>
+    	<!-- content -->
+    	<div style="margin-top:150px; margin-bottom:150px;" class="bg-dark"> 
+		<div class="container">
+		<h3>Berita Terbaru</h3>
+		<div class="garis"></div>
+		<!-- konten 1 -->
+			
+			<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-md-9">
+              <div class="panel panel-default">
+               <div class="panel-body">
+                  <h5>Webinar Memperingati Hari Pangan Sedunia</h5>
+                    <div class="info-meta">
+                        <ul class="list-inline">
+                            <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </li>
+                            <li><i class="fa fa-eye"></i>21k</li>
+                            <li><i class="fa fa-heart-o"></i>372</li>
+                            <li><i class="fa fa-user"></i> diposting oleh UMBandung</li>
+                            <li class="pull-right">Kategori : Kemahasiswaan</li>
+                        </ul>
+                    </div>
+                  <hr>
+                  
+                    <div class = "media">
+                       <a class = "pull-left" href = "#">
+                          <img class = "media-object " src = "gambar/kampus1.png" width="100%" >
+                       </a>
+                       <div class = "media-body">
+                       	<br><br>
+                          <p align="left">Peringati Hari Pangan Sedunia. UMB Bekerjasama Dengan Universitas Lain Adakan Webinar Akbar
+ Admin  Penelitian
+Hari Pangan Sedunia diperingati setiap tanggal 16 Oktober di setiap tahunnya. Untuk tahun ini, Universitas Muhammadiyah Bandung bekerja sama dengan Universitas Al-Azhar Indonesia dan Universitas Ahmad Dahlan melaksanakan webinar akbar berjudul World Food Day dengan tema "Menuju Pangan Berkelanjutan : Tantangan dan Inovasi" yang dilaksanakan pada sabtu kemarin tepatnya tanggal 6 November 2021.
+
+Webinar ini dipandu oleh dosen Teknologi Pangan Halal Universitas Muhammadiyah Bandung, Ratna Sari L, S.T.P, M.Si.</p><br><br><p align="left"> Webinar ini mendatangkan narasumber-narasumber yang ahli di bidang inovasi pengolahan pangan.Selanjutnya pemaparan materi oteh narasumber-narasumber yang dimoderatori oleh Titisari J, S.T.P., M.Sc., dosen Teknologi Pangan Universitas Ahmad Dahlan.  
+
+Narasumber yang dihadirkan diantaranya Prof. Leiwen Xiang dari Fujian Polytechnic Normal University, memaparkan materinya dengan judul "Tren Riset & Inovasi Terkini untuk Mendukung Sistem Pangan Berkelanjutan". Kemudian, dilanjutkan dengan pemaparan materi oleh Lisa Suhanda dari PT. Tetra Pak Indonesia dengan judul "Inovasi Teknologi Pengemasan untuk Mendukung Kemasan Berkelanjutan". Terakhir, ditutup dengan penyampaian dari Dr. Reza Abdul Jabbar, selaku Owner Large Scale Dairy Farm "Rural Practice Farm" Selandia Baru dengan judul materi "Elaborasi Masyarakat dan Industri Menuju Pangan Berkelanjutan".
+
+Materi yang telah disampaikan pada webinar kemarin, bisa diunduh di tautan di bawah ini :
+- Recent Research & Innovation Trends to Support Sustainable Food Systems - Prof. Leiwen Xiang (link)
+- Packaging Technology Innovation to Support Sustainable Packaging (link)
+                         </p>
+               <div class="col-md-3"> 
+               <div class="panel panel-default">
+               <div class="panel-heading">
+              </div>
+               <div class="panel-body">
+                    <div class="recent">
+                        <a href="#"><img class="img-responsive" src="" alt="" /></a>                
+                        <div class="info-meta">
+                            <ul class="list-inline">
+                                <li><i class="fa fa-clock-o"></i> Jan 22, 2022 </a></li>
+                                <li><i class="fa fa-eye"></i>21k</li>
+                                <li><i class="fa fa-heart-o"></i>372</li>
+                            </ul>
+                        </div>
+                        <h6 class="entry-tile">
+                            <a href="artikel-1.php"><font size="5px"> BEM UMBandung Resmi di Lantik</font></a>
+                        </h6>
+                    </div>
+                    
+                    
+                </div>
+                       </div>
+                        </div>
+                    <hr>
+                    <!--recent-->
+			<!-- berita 2 -->
+		</div>
+    
+    
+    	<!-- footer -->
+<div id="pantat" class="container bg-dark fixed-bottom">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="text-muted">kelompok 11 - kelompok 11 &copy; 2022</span>
+    </div>
+
+    <div id="pantat2" >
+      <a class="text-muted" href="#"><ion-icon name="logo-youtube"></ion-icon></a>
+      <a class="text-muted" href="#"><ion-icon name="logo-github"></ion-icon></a>
+      </div>
+  </footer>
+  </div>
+    </body>
+    </html>
